dced9fa605746f34ea1575d7b06231de6c16a81b
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2019-11-29 17:30:30 +0300

    Move deploy Cloud Functions on master branch.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Deploy to heroku and Cloud Function on commits of master branch 
 if [ "$TRAVIS_TAG" = "" ]; then
+    echo Deploy to heroku
     docker tag $DOCKER_USERNAME/$DOCKER_REPO registry.heroku.com/$HEROKU_REPO/web &&
     echo $HEROKU_API_KEY |
     docker login -u $HEROKU_IDENTITY --password-stdin registry.heroku.com &&
@@ -17,12 +19,6 @@ if [ "$TRAVIS_TAG" = "" ]; then
     -H "Content-Type: application/json" \
     -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
     -H "Authorization: $(echo -n $HEROKU_IDENTITY:$HEROKU_API_KEY | base64)"
-else
-    echo Deploy to docker hub
-    docker tag $DOCKER_USERNAME/$DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:$TRAVIS_TAG &&
-    echo $DOCKER_PASSWORD |
-    docker login -u $DOCKER_USERNAME --password-stdin &&
-    docker push $DOCKER_USERNAME/$DOCKER_REPO
     echo Deploy Cloud Function
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |
     sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list 
@@ -31,4 +27,12 @@ else
     sudo apt-get update && sudo apt-get install google-cloud-sdk
     gcloud auth activate-service-account --key-file=streamlink-1-1a849fdf22dd.json streamlink-1@appspot.gserviceaccount.com
     gcloud functions deploy --allow-unauthenticated --trigger-http --runtime=python37 --region=us-east1 --project=streamlink-1 streamlink
+
+# Deploy to docker hub new version (tag)
+else
+    echo Deploy to docker hub
+    docker tag $DOCKER_USERNAME/$DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:$TRAVIS_TAG &&
+    echo $DOCKER_PASSWORD |
+    docker login -u $DOCKER_USERNAME --password-stdin &&
+    docker push $DOCKER_USERNAME/$DOCKER_REPO
 fi
