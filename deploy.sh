e5206f973c7b28351195394c9e55f882fa6e01df
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2019-11-29 17:13:27 +0300

    Add deploy Cloud Functions

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,17 @@ if [ "$TRAVIS_TAG" = "" ]; then
     -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
     -H "Authorization: $(echo -n $HEROKU_IDENTITY:$HEROKU_API_KEY | base64)"
 else
+    echo Deploy to docker hub
     docker tag $DOCKER_USERNAME/$DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:$TRAVIS_TAG &&
     echo $DOCKER_PASSWORD |
     docker login -u $DOCKER_USERNAME --password-stdin &&
     docker push $DOCKER_USERNAME/$DOCKER_REPO
+    echo Deploy Cloud Function
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |
+    sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list 
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |
+    sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    sudo apt-get update && sudo apt-get install google-cloud-sdk
+    gcloud auth activate-service-account --key-file=streamlink-1-1a849fdf22dd.json streamlink-1@appspot.gserviceaccount.com
+    gcloud functions deploy --allow-unauthenticated --trigger-http --runtime=python37 --region=us-east1 --project=streamlink-1 streamlink
 fi
