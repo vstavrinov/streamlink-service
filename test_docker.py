747370f32cc843280d8bb28e7953712430c94ddc
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2020-11-05 17:46:34 +0300

    Back to sleep()

--- test_docker.py
+++ test_docker.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 import query_cases
+from time import sleep
 
 endpoint = 'http://localhost:8000/?'
 
@@ -7,4 +8,5 @@ endpoint = 'http://localhost:8000/?'
 class TestQuery(query_cases.TestCases):
 
     def probe(self, args, count):
+        sleep(1)
         return urlopen(endpoint + args).read(count)
