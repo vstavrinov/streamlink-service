6f7081c3d106c3058eae591554b0adac41764510
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-01-17 21:01:52 +0300

    set docker tag to git tag or streamlink sha

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,44 @@
 #!/usr/bin/env bash
 
-# Deploy to heroku and Cloud Function on commits of master branch 
+# Deploy to heroku and Cloud Function on commits of master branch
+if [ "${GITHUB_REF#refs/tags/}" != "$GITHUB_REF" ]; then
+    TAG=${GITHUB_REF#refs/tags/}
+elif [ -n "$SHA" ]; then
+    TAG=$SHA
+else
+    TAG="latest"
+fi
+IMAGE=$(docker inspect registry.heroku.com/$HEROKU_REPO/web --format={{.Id}})
 echo Deploy to heroku
 echo "$HEROKU" |
 while read HEROKU_REPO HEROKU_IDENTITY HEROKU_API_KEY; do
     if [ -n "$HEROKU_REPO" ]; then
-        docker tag $DOCKER_USERNAME/$DOCKER_REPO:${GITHUB_REF#refs/*/} registry.heroku.com/$HEROKU_REPO/web &&
+        docker tag $DOCKER_USERNAME/$DOCKER_REPO \
+            registry.heroku.com/$HEROKU_REPO/web &&
         echo $HEROKU_API_KEY |
-        docker login -u $HEROKU_IDENTITY --password-stdin registry.heroku.com &&
+        docker login -u $HEROKU_IDENTITY --password-stdin \
+            registry.heroku.com &&
         docker push registry.heroku.com/$HEROKU_REPO/web &&
         curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_REPO/formation \
-         -d '{
-          "updates": [
-            {
-              "type": "web",
-              "docker_image": "'$(docker inspect registry.heroku.com/$HEROKU_REPO/web --format={{.Id}})'"
-            }
-          ]
-        }' \
+            -d '{
+                "updates": [
+                    {
+                        "type": "web",
+                        "docker_image": "'$IMAGE'"
+                    }
+                ]
+            }' \
         -H "Content-Type: application/json" \
         -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
-        -H "Authorization: $(echo -n $HEROKU_IDENTITY:$HEROKU_API_KEY | base64 -w0)"
+        -H "Authorization: $(echo -n $HEROKU_IDENTITY:$HEROKU_API_KEY |
+        base64 -w0)"
     fi
 done
 # Deploy to docker hub new version (tag)
 echo Deploy to docker hub
 echo $DOCKER_PASSWORD |
 docker login -u $DOCKER_USERNAME --password-stdin &&
-docker push $DOCKER_USERNAME/$DOCKER_REPO:${GITHUB_REF#refs/*/}
+docker push $DOCKER_USERNAME/$DOCKER_REPO:$TAG
+if [ "$TAG" != "latest" ]; then
+    docker push $DOCKER_USERNAME/$DOCKER_REPO:latest
+fi
