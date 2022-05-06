6ac7de9fc9de4412edd474f681d24acd26305b02
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-05-06 20:56:36 +0300

    deploy: remove && operator to make errexit option working

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-shopt -s inherit_errexit
-
 # Deploy to heroku and Cloud Function on commits of master branch
 if [ "${GITHUB_REF#refs/tags/}" != "$GITHUB_REF" ]; then
     TAG=${GITHUB_REF#refs/tags/}
@@ -16,11 +14,11 @@ echo "$HEROKU" |
 while read HEROKU_REPO HEROKU_IDENTITY HEROKU_API_KEY; do
     if [ -n "$HEROKU_REPO" ]; then
         docker tag $DOCKER_USERNAME/$DOCKER_REPO \
-            registry.heroku.com/$HEROKU_REPO/web &&
+            registry.heroku.com/$HEROKU_REPO/web
         echo $HEROKU_API_KEY |
         docker login -u $HEROKU_IDENTITY --password-stdin \
-            registry.heroku.com &&
-        docker push registry.heroku.com/$HEROKU_REPO/web &&
+            registry.heroku.com
+        docker push registry.heroku.com/$HEROKU_REPO/web
         curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_REPO/formation \
             -d '{
                 "updates": [
@@ -40,7 +38,7 @@ done
 echo Deploy to docker hub
 docker tag $DOCKER_USERNAME/$DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO:$TAG
 echo $DOCKER_PASSWORD |
-docker login -u $DOCKER_USERNAME --password-stdin &&
+docker login -u $DOCKER_USERNAME --password-stdin
 docker push $DOCKER_USERNAME/$DOCKER_REPO:$TAG
 if [ "$TAG" != "latest" ]; then
     docker push $DOCKER_USERNAME/$DOCKER_REPO:latest
