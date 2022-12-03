7015871ade00f599dfcfb8d4b3f2ead779cb5f1c
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-12-03 18:10:50 +0300

    - sweep off heroku leftovers - echoing TAG and refs

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
 
-# Deploy to heroku and Cloud Function on commits of master branch
 TAG=$(versioningit)
 # Deploy to docker hub new version (tag)
-echo Deploy to docker hub
+echo Deploy to docker hub new version GITHUB_REF=${GITHUB_REF}, TAG=$TAG,  GITHUB_REF_NAME=$GITHUB_REF_NAME
 echo $DOCKER_PASSWORD |
 docker login -u $DOCKER_USERNAME --password-stdin
 docker tag $DOCKER_USERNAME/$DOCKER_REPO{,:$TAG}
