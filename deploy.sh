112f3cc6408b961756a9817df9e3178d80baa151
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2022-12-03 16:59:24 +0300

    remove heroku as closed

--- deploy.sh
+++ deploy.sh
@@ -2,32 +2,6 @@
 
 # Deploy to heroku and Cloud Function on commits of master branch
 TAG=$(versioningit)
-IMAGE=$(docker inspect $DOCKER_USERNAME/$DOCKER_REPO --format={{.Id}})
-echo Deploy to heroku
-echo "$HEROKU" |
-while read HEROKU_REPO HEROKU_IDENTITY HEROKU_API_KEY; do
-    if [ -n "$HEROKU_REPO" ]; then
-        docker tag $DOCKER_USERNAME/$DOCKER_REPO \
-            registry.heroku.com/$HEROKU_REPO/web
-        echo $HEROKU_API_KEY |
-        docker login -u $HEROKU_IDENTITY --password-stdin \
-            registry.heroku.com
-        docker push registry.heroku.com/$HEROKU_REPO/web
-        curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_REPO/formation \
-            -d '{
-                "updates": [
-                    {
-                        "type": "web",
-                        "docker_image": "'$IMAGE'"
-                    }
-                ]
-            }' \
-        -H "Content-Type: application/json" \
-        -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
-        -H "Authorization: $(echo -n $HEROKU_IDENTITY:$HEROKU_API_KEY |
-        base64 -w0)"
-    fi
-done
 # Deploy to docker hub new version (tag)
 echo Deploy to docker hub
 echo $DOCKER_PASSWORD |
